6d4c9994f9eb37ee4000792092fd8eb4d92f3ac1
shiyas ps <shiyasofficial1@gmail.com>
2025-10-26 13:19:49 +0000

    tjsid

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 // Get your button
 const actionBtn = document.getElementById('actionBtn'); // or however you select it
 
-   const button = document.getElementById('downloadButton');
+  const button = document.getElementById('downloadButton');
 
   button.addEventListener('click', async () => {
     try {
@@ -10,11 +10,17 @@ const actionBtn = document.getElementById('actionBtn'); // or however you select
       if (availability === 'downloadable' || availability === 'downloading') {
         console.log('Downloading model — please wait...');
         await LanguageModel.create({
+          expectedInputs: [
+            { type: "text", languages: ["en"] } // input from system/user is English
+          ],
+          expectedOutputs: [
+            { type: "text", languages: ["en"] } // specify desired output language
+          ],
           monitor(m) {
-            m.addEventListener('downloadprogress', (e) => {
+            m.addEventListener("downloadprogress", (e) => {
               console.log(`Progress: ${(e.loaded * 100).toFixed(1)}%`);
             });
-          },
+          }
         });
         console.log('Model downloaded and ready to use!');
       } else if (availability === 'available') {
